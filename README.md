5f3711cbf404cb8465f9b1b39367e879c014f483
ryowk <r.wakatsuki1207@gmail.com>
2021-09-03 21:47:25 +0900

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# kintai
+
+## Format
+```json
+{"2020/01/01": 1.2, "2020/01/02": 0.0}
+```
+
+## Secrets
+* `SLACK_TOKEN`
+* `SLACK_CHANNEL`
---
+++ main.py
@@ -0,0 +1,110 @@
+import calendar
+import datetime
+import json
+import os
+from pathlib import Path
+from typing import NamedTuple
+
+from logzero import logger
+from slack_sdk import WebClient
+
+JST = datetime.timezone(datetime.timedelta(hours=+9), 'JST')
+
+START_TEXTS = ['開始']
+END_TEXTS = ['終了']
+
+
+class Record(NamedTuple):
+    dt: datetime.datetime
+    kind: bool
+
+
+def list_all_dates(year: int, month: int) -> list[str]:
+    dates = []
+    for day in range(32):
+        try:
+            dates.append(datetime.datetime(year, month, day).date())
+        except ValueError:
+            pass
+    return dates
+
+
+def calc_hours(date: datetime.date, records: list[NamedTuple]) -> float:
+    if len(records) == 0:
+        return 0
+    if records[0].kind:
+        records.insert(0, Record(datetime.datetime(date.year, date.month, date.day, 0, 0, 0, tzinfo=JST), False))
+    if not records[-1].kind:
+        records.append(Record(datetime.datetime(date.year, date.month, date.day, 23, 59, 59, tzinfo=JST), True))
+
+    total_seconds = 0
+
+    for i in range(0, len(records), 2):
+        record_start = records[i]
+        record_end = records[i + 1]
+        assert not record_start.kind
+        assert record_end.kind
+        seconds = (record_end.dt - record_start.dt).total_seconds()
+        total_seconds += seconds
+    return total_seconds / 3600
+
+
+def main():
+    now = datetime.datetime.now(JST)
+    year = now.year
+    month = now.month
+    first_date_of_this_month = datetime.datetime(year, month, 1, tzinfo=JST)
+    last_date_of_last_month = first_date_of_this_month - datetime.timedelta(days=1)
+    prev_year = last_date_of_last_month.year
+    prev_month = last_date_of_last_month.month
+
+    targets = [(year, month), (prev_year, prev_month)]
+
+    for year, month in targets:
+        logger.info(f"target: {year}-{month:02}")
+
+        dt_start = datetime.datetime(year, month, 1, 0, 0, 0, 0, tzinfo=JST)
+        dt_end = datetime.datetime(year, month, calendar.monthrange(year, month)[1], 23, 59, 59, 999999, tzinfo=JST)
+
+        client = WebClient(token=os.environ['SLACK_TOKEN'])
+
+        response = client.conversations_history(
+            channel=os.environ['SLACK_CHANNEL'],
+            oldest=dt_start.timestamp(),
+            latest=dt_end.timestamp(),
+        )
+        records = []
+        for x in response['messages']:
+            text = x['text']
+            ts = x['ts']
+            if text not in START_TEXTS + END_TEXTS:
+                continue
+
+            kind = text in END_TEXTS
+
+            dt = datetime.datetime.fromtimestamp(int(float(ts)), tz=JST)
+            records.append(Record(dt, kind))
+        records = sorted(records)
+
+        # validate records
+        for i in range(len(records) - 1):
+            assert records[i].kind != records[i + 1].kind
+
+        all_dates = list_all_dates(year, month)
+        dt_to_records = {date: [] for date in all_dates}
+        for x in records:
+            dt_to_records[x.dt.date()].append(Record(x.dt, x.kind))
+
+        summary = {}
+        for date in all_dates:
+            hours = calc_hours(date, dt_to_records[date])
+            summary[str(date)] = hours
+
+        save_path = Path('./docs/data') / f'{year}-{month:02}.json'
+        with open(save_path, 'w') as f:
+            json.dump(summary, f)
+        logger.info(f"saved {save_path}")
+
+
+if __name__ == '__main__':
+    main()
